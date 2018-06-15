f9e6dbced6452330bbf0a298d4ca366767cbce2f
hupe1980 <hupe1980@users.noreply.github.com>
2018-06-15 07:10:14 +0200

    feat: Misc

--- packages/react-contentful/src/Contentful.js
+++ packages/react-contentful/src/Contentful.js
@@ -7,27 +7,74 @@ import { ContentfulContext } from './context';
 const propTypes = {
   space: PropTypes.string.isRequired,
   environment: PropTypes.string,
-  accessToken: PropTypes.string,
+  accessToken: PropTypes.string.isRequired,
+  insecure: PropTypes.bool,
+  host: PropTypes.string,
+  basePath: PropTypes.string,
+  httpAgent: PropTypes.object,
+  httpsAgent: PropTypes.object,
+  proxy: PropTypes.object,
+  headers: PropTypes.object,
+  resolveLinks: PropTypes.bool,
+  removeUnresolved: PropTypes.bool,
+  retryOnError: PropTypes.bool,
+  logHandler: PropTypes.func,
+  application: PropTypes.string,
+  integration: PropTypes.string,
+  timeout: PropTypes.number,
 };
 
 const defaultProps = {
   environment: 'master',
+  insecure: true,
+  host: 'cdn.contentful.com',
+  basePath: '',
+  httpAgent: undefined,
+  httpsAgent: undefined,
+  proxy: undefined,
+  headers: {},
+  resolveLinks: true,
+  removeUnresolved: false,
+  retryOnError: true,
+  logHandler: (level, data) => {},
+  application: '',
+  integration: '',
+  timeout: 30000,
 };
 
 class Contentful extends Component {
   constructor(props) {
     super(props);
 
-    const { space, environment, accessToken, ...rest } = props;
-
-    this.client = createClient({
-      space,
-      environment,
-      accessToken,
-    });
-
-    this.rest = rest;
+    const params = this.getParamsFromProps(props);
+    this.client = createClient(params);
   }
+
+  getParamsFromProps = props =>
+    [
+      'space',
+      'environment',
+      'accessToken',
+      'insecure',
+      'host',
+      'basePath',
+      'httpAgent',
+      'httpsAgent',
+      'proxy',
+      'headers',
+      'resolveLinks',
+      'removeUnresolved',
+      'retryOnError',
+      'logHandler',
+      'application',
+      'integration',
+      'timeout',
+    ].reduce((options, key) => {
+      if (props[key]) {
+        options[key] = props[key];
+      }
+      return options;
+    }, {});
 
   render() {
     const { children } = this.props;
--- packages/react-google-maps/README.md
+++ packages/react-google-maps/README.md
@@ -1,7 +1,7 @@
 # react-google-maps
 [![Build Status](https://travis-ci.org/hupe1980/wapps-components.svg?branch=master)](https://travis-ci.org/hupe1980/wapps-components)
 
-React google maps component
+React google maps components
 
 ## Example
 ```js
@@ -30,71 +30,3 @@ For a demo, check out https://hupe1980.github.io/wapps-components/
 
 ## Installation
 - `npm install --save @wapps/react-google-maps`
-
-### Marker
-To place a marker on the Map, include it as a child of the <Map /> component.
-
-```js
-import React from 'react';
-import GoogleMaps, { Map, Marker } from '@wapps/react-google-maps';
-
-const App = () => (
-  <GoogleMaps api={{ key: YOUR_googleMaps_KEY }}>
-    <div style={{ height: '100vh', width: '100wh'}}>
-      <Map
-        center={{
-          lat: 52.520008,
-          lng: 13.404954,
-        }}
-        zoom={15}
-      >
-        <Marker
-          position={{
-            lat: 52.520008,
-            lng: 13.404954,
-          }}
-        />
-      </Map>
-    </div>
-  </GoogleMaps>
-);
-
-export default App;
-```
-
-### OverlayView
-To place a overlay on the Map, include it as a child of the <Map /> component.
-
-```js
-import React from 'react';
-import GoogleMaps, { Map, OverlayView } from '@wapps/react-google-maps';
-
-const App = () => (
-  <GoogleMaps api={{ key: YOUR_googleMaps_KEY }}>
-    <div style={{ height: '100vh', width: '100wh'}}>
-      <Map
-        center={{
-          lat: 52.520008,
-          lng: 13.404954,
-        }}
-        zoom={15}
-      >
-      <OverlayView
-        position={{
-          lat: 52.520008,
-          lng: 13.404954,
-        }}
-      >
-        <div
-          style={{ backgroundColor: 'blue', color: 'white', padding: '15px' }}
-        >
-          Hello!
-        </div>
-      </OverlayView>
-      </Map>
-    </div>
-  </GoogleMaps>
-);
-
-export default App;
-```
