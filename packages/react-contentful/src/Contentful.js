6ff3b160a725fcc37489fc9c1191376f3d22b2f1
hupe1980 <hupe1980@users.noreply.github.com>
2018-06-15 09:10:28 +0200

    feat: Misc

--- packages/react-contentful/src/Contentful.js
+++ packages/react-contentful/src/Contentful.js
@@ -26,7 +26,7 @@ const propTypes = {
 
 const defaultProps = {
   environment: 'master',
-  insecure: true,
+  insecure: false,
   host: 'cdn.contentful.com',
   basePath: '',
   httpAgent: undefined,
